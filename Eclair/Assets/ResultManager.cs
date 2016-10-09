8e509c98a2c26bb5abdf0b0d170546a3f26af252
wararyo <wararyo@gmail.com>
2016-10-10 00:02:27 +0900

    数字部分実装

--- Eclair/Assets/ResultManager.cs
+++ Eclair/Assets/ResultManager.cs
@@ -13,10 +13,18 @@ public class ResultManager : MonoBehaviour {
 	//かなり頭悪いコード書くけど許して…！
 	KeyValuePair<float,string>[] timeline = {
 		new KeyValuePair<float,string>(0.5f,"Frame"),
-		new KeyValuePair<float,string>(0.5f,"Title"),
+		new KeyValuePair<float,string>(0.4f,"Title"),
 		new KeyValuePair<float,string>(0.5f,"TimeLabel"),
+		new KeyValuePair<float,string>(0.5f,"TimeText"),
+		new KeyValuePair<float,string>(0.5f,"TimeScore"),
 		new KeyValuePair<float,string>(0.5f,"EnemyLabel"),
-		new KeyValuePair<float,string>(0.5f,"BoltLabel")
+		new KeyValuePair<float,string>(0.5f,"EnemyText"),
+		new KeyValuePair<float,string>(0.5f,"EnemyScore"),
+		new KeyValuePair<float,string>(0.5f,"BoltLabel"),
+		new KeyValuePair<float,string>(0.5f,"BoltText"),
+		new KeyValuePair<float,string>(0.5f,"BoltScore"),
+		new KeyValuePair<float,string>(0.5f,"TotalLabel"),
+		new KeyValuePair<float,string>(0.5f,"TotalText"),
 	};
 
 	// Use this for initialization
@@ -30,7 +38,7 @@ public class ResultManager : MonoBehaviour {
 			return;
 		if (timeline [cursor].Key < time) {
 			GameObject.Find (timeline [cursor].Value).GetComponents<AnimationQueueBase>()[0].Queue ();
-			Debug.Log (timeline [cursor].Value);
+			//Debug.Log (timeline [cursor].Value);
 			time = 0;
 			cursor++;
 		}
--- Eclair/Assets/Scripts/AnimationQueue_Number.cs
+++ Eclair/Assets/Scripts/AnimationQueue_Number.cs
@@ -1,19 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class AnimationQueue_Number : AnimationQueueBase {
 
+	private Text text;
+	private int currentNumber = 0;
+	public int number = 10;
+	public int step = 1;
+	public bool useSign = false;
+
 	// Use this for initialization
 	void Start () {
-	
+		text = GetComponent<Text> ();
+		text.enabled = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (text.enabled && currentNumber < number) {
+			currentNumber += step;
+			setText (currentNumber);
+		} else if (currentNumber > number) {
+			setText (number);
+		}
 	}
 
 	public override void Queue(){
+		text.enabled = true;
+		text.text = "0";
+	}
 
+	private void setText(int num){
+		text.text = num.ToString ();
+		if (num > 0 && useSign)
+			text.text = text.text.Insert (0, "+");
 	}
 }
+
