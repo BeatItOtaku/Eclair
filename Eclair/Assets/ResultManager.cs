f9cfcff10eebf6a80a897292743e8d42286bec50
wararyo <wararyo@gmail.com>
2016-10-09 22:56:08 +0900

    :new: ResultManager
    timeline変数の中身に従ってアニメーションが行われていく感じやで

---
+++ Eclair/Assets/ResultManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ResultManager : MonoBehaviour {
+
+	public GameObject frame;
+	public GameObject title;
+
+	private float time = 0;
+	private int cursor = 0;
+
+	//かなり頭悪いコード書くけど許して…！
+	KeyValuePair<float,string>[] timeline = {
+		new KeyValuePair<float,string>(0.5f,"Frame"),
+		new KeyValuePair<float,string>(0.5f,"Title"),
+		new KeyValuePair<float,string>(0.5f,"TimeLabel"),
+		new KeyValuePair<float,string>(0.5f,"EnemyLabel"),
+		new KeyValuePair<float,string>(0.5f,"BoltLabel")
+	};
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (timeline.Length == cursor)
+			return;
+		if (timeline [cursor].Key < time) {
+			GameObject.Find (timeline [cursor].Value).GetComponents<AnimationQueueBase>()[0].Queue ();
+			Debug.Log (timeline [cursor].Value);
+			time = 0;
+			cursor++;
+		}
+	}
+}
---
+++ Eclair/Assets/Scripts/AnimationQueue_Number.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationQueue_Number : AnimationQueueBase {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void Queue(){
+
+	}
+}
