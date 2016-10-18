fa4b6559c0a6efa648f5ddea22b4a6a73fe28abc
wararyo <wararyo@gmail.com>
2016-10-19 01:23:38 +0900

    :new: EnemyBase
    すべての敵の継承元となるクラス、EnemyBaseクラスを作ったよ

---
+++ Eclair/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class EnemyBase : MonoBehaviour {
+
+	public int HP;
+	public int maxHp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// ダメージを食らいます。
+	/// 具体的には中のHPプロパティが減ります。
+	/// </summary>
+	/// <param name="damage">Damage.</param>
+	public abstract void Damage (int damage);
+}
