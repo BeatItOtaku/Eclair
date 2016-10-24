d6f326cf89f8703eda2a61e591740d0f99cd3942
wararyo <wararyo@gmail.com>
2016-10-24 17:38:28 +0900

    エクレアについてくるウミガメの卵

--- Eclair/Assets/Scripts/EnemyNormal.cs
+++ Eclair/Assets/Scripts/EnemyNormal.cs
@@ -6,14 +6,28 @@ public class EnemyNormal : EnemyBase {
 	public int maxHp = 16;
 	public int HP{ get; set; }
 
+	public GameObject player;
+
+	/// <summary>
+	/// エクレアを感知する距離
+	/// </summary>
+	public float searchDistance = 16;
+
 	// Use this for initialization
 	void Start () {
 		HP = maxHp;
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Vector3.Distance (player.transform.position, transform.position) < searchDistance) {
+			//プレイヤーが近づいてる時
+			transform.LookAt(player.transform.position);
+			transform.position += transform.forward * Time.deltaTime;
+		} else {
+			transform.position += transform.forward * Time.deltaTime;
+		}
 	}
 
 	public override void Damage(int damage,Vector3 direction){
