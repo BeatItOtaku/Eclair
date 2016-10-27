2ad791584d2979d760178f046ce441c37ac02d84
wararyo <wararyo@gmail.com>
2016-10-27 18:31:37 +0900

    調整
    いろいろやったよ

--- Eclair/Assets/Scripts/EnemyNormal.cs
+++ Eclair/Assets/Scripts/EnemyNormal.cs
@@ -6,7 +6,10 @@ public class EnemyNormal : EnemyBase {
 	public int maxHp = 15;
     public int attackReaction = 4;
     public int damageReaction = 6;
+	public int dieReaction = 2;
 	public int HP{ get; set; }
+	public float normalSpeed = 2;
+	public float attackSpeed = 4;
 
 	public GameObject player;
     private Animator anim;
@@ -29,24 +32,27 @@ public class EnemyNormal : EnemyBase {
             //プレイヤーが近づいてる時
             anim.SetBool("isAttacking", true);
 			transform.LookAt(player.transform.position);
-			transform.position += transform.forward * Time.deltaTime;
+			transform.position += transform.forward * attackSpeed * Time.deltaTime;
 		} else {
             anim.SetBool("isAttacking", false);
-            transform.position += transform.forward * Time.deltaTime;
+			transform.position += transform.forward * normalSpeed * Time.deltaTime;
 		}
 	}
 
 	public override void Damage(int damage,Vector3 direction){
 		HP -= damage;
+		GetComponent<Rigidbody>().velocity = (-transform.forward * damageReaction);
         if (HP <= 0)
         {
             anim.SetTrigger("Died");
-            Destroy(gameObject, 1.0f);
+			GetComponent<Rigidbody> ().constraints = new RigidbodyConstraints ();
+			GetComponent<Rigidbody> ().velocity += transform.up * dieReaction;
+            Destroy(gameObject, 1.2f);
         }
         else
         {
             anim.SetTrigger("Damaged");
-            GetComponent<Rigidbody>().AddForce(-transform.forward * damageReaction, ForceMode.VelocityChange);
+
         }
         Debug.Log ("ZakoHP:" + HP);
 
@@ -57,7 +63,7 @@ public class EnemyNormal : EnemyBase {
 		if(col.gameObject.CompareTag("Player")){
             anim.SetTrigger("Attack");
 			col.gameObject.GetComponent<PlayerControl> ().Damage (5);
-            GetComponent<Rigidbody>().AddForce(-transform.forward * attackReaction, ForceMode.VelocityChange);
+			GetComponent<Rigidbody>().velocity = (-transform.forward * attackReaction);
 		}
 	}
 }
