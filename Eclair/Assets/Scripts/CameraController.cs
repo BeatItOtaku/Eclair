ca3890567d7956f62f7803a8456b5b613d0d7c4e
wararyo <wararyo@gmail.com>
2016-11-02 22:29:25 +0900

    常にプレイヤーが見えるカメラ移動
    プレイヤーとカメラの間に物体があったら近づくようになったぜ
    ついでにLockOnしやすくした

--- Eclair/Assets/Scripts/CameraController.cs
+++ Eclair/Assets/Scripts/CameraController.cs
@@ -163,16 +163,28 @@ public class CameraController : MonoBehaviour
 
 
 	private void setCameraPosition(float y, float angle){
-			//if (angle > maxAngle) angle = maxAngle;
-			//if (angle < minAngle) angle = minAngle;
+        //if (angle > maxAngle) angle = maxAngle;
+        //if (angle < minAngle) angle = minAngle;
 
+        float mDist = distance;//プレイヤーとカメラの間に物体があった時はmDistが小さくなる
+        
 
-		cameraOffset = new Vector3(0, 0, -distance);
+		cameraOffset = new Vector3(0, 0, -1);
 		cameraOffset = Quaternion.Euler(angle, y, 0) * cameraOffset;
 
-		Transform lookAtTransform = lookAt.GetComponent<Transform>();
+        Transform lookAtTransform = lookAt.GetComponent<Transform>();
+
+        RaycastHit hit;
+        if (Physics.Raycast(lookAtTransform.position, cameraOffset,out hit, distance))
+        {
+            mDist = Vector3.Distance(lookAtTransform.position, hit.point);
+        }
+
+        cameraOffset *= mDist;
+
 		cameraTransform.position = lookAtTransform.position + cameraOffset;
 
+        //カメラはLookAtに指定したオブジェクトよりちょっと上を向く
 		cameraTransform.LookAt(lookAt.GetComponent<Transform>().position);
 		cameraTransform.RotateAround (cameraTransform.position, cameraTransform.right, -cameraVerticalAngleOffset);
 	}
