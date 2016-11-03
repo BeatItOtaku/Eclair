26de76c617da491c50e14cceb078616a3b7ea5d3
wararyo <wararyo@gmail.com>
2016-11-03 15:21:59 +0900

    :new: AutoButtonPress

---
+++ Eclair/Assets/Scripts/AutoButtonPress.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoButtonPress : MonoBehaviour {
+
+	public float timeToPress = 8;
+	private float timeCursor = 0;
+
+	public bool enableWhenAwake = true;
+	private bool isCounting = false;
+
+	public Image progress;
+
+	// Use this for initialization
+	void Start () {
+		if (enableWhenAwake)
+			startCount ();
+		//progress = GetComponentInChildren
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isCounting) {
+			timeCursor += Time.deltaTime;
+			if (timeCursor >= timeToPress) {
+				GetComponent<Button> ().onClick.Invoke();
+			} else {
+				progress.fillAmount = timeCursor / timeToPress;
+			}
+		}
+	}
+
+	public void startCount(){
+		isCounting = true;
+		timeCursor = 0;
+	}
+}
