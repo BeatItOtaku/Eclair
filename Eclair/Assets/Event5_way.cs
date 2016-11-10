3b2efa9715f48eb01e077baf6dc1e1f9c73bcf69
naotee <naotee917@gmail.com>
2016-11-10 16:21:24 +0900

    調整

---
+++ Eclair/Assets/Event5_way.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event5_way : MonoBehaviour {
+
+	public GameObject fire;
+
+	public EventManager2 em2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (em2.eventCount2 == 5) {
+			Instantiate (fire, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	
+	}
+}
