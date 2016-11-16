e8f42b2255d5c095d9d336b4471e6a6e2e3536e9
naotee <naotee917@gmail.com>
2016-11-16 12:16:10 +0900

    BossBarret修正

--- Eclair/Assets/Scripts/BossBarret.cs
+++ Eclair/Assets/Scripts/BossBarret.cs
@@ -12,10 +12,12 @@ public class BossBarret : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
-		/*if (player == null) {	
+		if (player == null) {	
 			player = GameObject.FindGameObjectWithTag ("ETOEclair");
-		} */
-		transform.LookAt (player.transform.FindChild("Muzzle"));
+			transform.LookAt (player.transform);
+		} else {
+			transform.LookAt (player.transform.FindChild ("Muzzle"));
+		}
 
 	}
 
