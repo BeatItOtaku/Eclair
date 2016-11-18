255be2af90bbbc23b4accab524d86da29abd9391
wararyo <wararyo@gmail.com>
2016-11-19 01:59:29 +0900

    :bug: ボスに行かない

--- Eclair/Assets/EventManager2.cs
+++ Eclair/Assets/EventManager2.cs
@@ -28,6 +28,7 @@ public class EventManager2 : MonoBehaviour {
 	public GameObject event7_bikkuri;
 	public GameObject event9_yajirushi;
 	public Camera event9_camera;
+	public GameObject event10_way;
 
     public BossGate bg;
 
@@ -111,6 +112,7 @@ public class EventManager2 : MonoBehaviour {
     {
         yield return StartCoroutine(CameraChanger.Instance.ChangeTemporary_raw(event9_camera, 5));
 		event9_yajirushi.SetActive (true);
+		event10_way.SetActive (true);
     }
 
     public void BossSwitch(){
